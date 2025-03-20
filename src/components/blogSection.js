0022b63c52de16a2bde9e8bdd8196461cb249865
Asif9024 <aasifansar9024@gmail.com>
2025-03-20 09:22:32 +0530

    made responsive to home page

--- src/components/blogSection.js
+++ src/components/blogSection.js
@@ -341,7 +341,7 @@ const BlogSection = () => {
               <motion.div
                 initial={{ opacity: 0, y: 50 }}
                 whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.4, ease: "easeOut" }}
+                transition={{ duration: 0.9, ease: "easeOut" }}
                 viewport={{ once: true }}
               >
                 <img
